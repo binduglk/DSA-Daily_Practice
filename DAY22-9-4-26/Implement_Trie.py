48a4048173ddc73f03600d58bd254393858378d0
binduglk <binduglk@gmail.com>
2026-04-09 19:46:11 +0530

    Initial Commit

---
+++ DAY22-9-4-26/Implement_Trie.py
@@ -0,0 +1,46 @@
+# Session 1 - Implement Trie
+# LeetCode #208
+# Topic - Trie
+# Day 19
+
+class TrieNode:
+    def __init__(self):   # corrected
+        self.children = {}
+        self.is_end = False
+
+class Trie:
+    def __init__(self):   # corrected
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+# Testing
+trie = Trie()
+trie.insert("apple")
+print(trie.search("apple"))      # True
+print(trie.search("app"))        # False
+print(trie.startsWith("app"))    # True
+trie.insert("app")
+print(trie.search("app"))        # True
